a1680a40ede6c155054761b4b07d85fbe0fd2302
sabbirirfan <sabbirirfan29@gmail.com>
2024-07-02 18:49:30 +0600

    keri-back

---
+++ prov_learning/keri_project/keri_backend/src/test/java/org/dsi/keriback/KeriBackApplicationTests.java
@@ -0,0 +1,13 @@
+package org.dsi.keriback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KeriBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
