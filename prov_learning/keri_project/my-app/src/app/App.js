07887da7bfc6485267ac9ad48ddd891214328ac0
sabbirirfan <sabbirirfan29@gmail.com>
2024-07-04 19:31:42 +0600

    signify added success

---
+++ prov_learning/keri_project/my-app/src/app/App.js
@@ -0,0 +1,18 @@
+
+import OtpForm from './component/otpPage.jsx';
+import Container from 'react-bootstrap/Container';
+
+const App = () => {
+  return (
+    <>
+
+      <Container style={{width: "50%", marginTop: "100px"}}>
+        
+        <OtpForm />
+      </Container>
+    
+    </>
+  );
+}
+
+export default App;
