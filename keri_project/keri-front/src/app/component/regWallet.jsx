495a4de65b3d12b2f986448377d50304196b7aa0
SabbirIrfan <50948678+SabbirIrfan@users.noreply.github.com>
2024-07-18 13:53:55 +0600

    Delete keri_project directory

--- keri_project/keri-front/src/app/component/regWallet.jsx
+++
@@ -1,7 +0,0 @@
-import { Container } from "postcss";
-import React from "react";
-import { SignifyClient } from "signify-ts";
-
-export const regWallet = () => {
-  return <Container>regWallet</Container>;
-};
