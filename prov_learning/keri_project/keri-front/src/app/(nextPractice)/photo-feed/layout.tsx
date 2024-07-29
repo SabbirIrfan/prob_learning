b452259ebb78d83b009bbc305056a04e17c8699d
sabbirirfan <sabbirirfan29@gmail.com>
2024-07-29 14:34:36 +0600

    ROUTE HANDLER ADDED.

--- prov_learning/keri_project/keri-front/src/app/(nextPractice)/photo-feed/layout.tsx
+++ prov_learning/keri_project/keri-front/src/app/(nextPractice)/photo-feed/layout.tsx
@@ -6,7 +6,13 @@ export default function Layout(props: {
 }) {
   return (
     <>
-      {props.modal}
+     {/* how does this invoked ? 
+        the naming convention of @modal is the parallel routing.
+         so when the layout is intiated, they will expect props of all parralel routs
+     */}
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                    
+      {props.modal} 
+     
       {props.children}
     </>
   );
--- prov_learning/keri_project/keri-front/src/app/(nextPractice)/photo-feed/page.tsx
+++ prov_learning/keri_project/keri-front/src/app/(nextPractice)/photo-feed/page.tsx
@@ -6,11 +6,11 @@ export default function Home() {
   return (
     <main className="container mx-auto">
       <h1 className="text-center text-3xl font-bold my-4">
-        New Wonders of the World
+        New What of the World?
       </h1>
       <div className="grid grid-cols-1 md:grid-cols-4 gap-4">
         {wonders.map(({ id, src, name }) => (
-          <Link key={id} href={`/photo-feed/${id}`}>
+          <Link key={id} href={`${id}`}>
             <Image
               alt={name}
               src={src}
