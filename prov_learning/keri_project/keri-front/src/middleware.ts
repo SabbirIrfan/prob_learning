03ca8549eeaeb0fe1228e579d57c815e8b21417a
sabbirirfan <sabbirirfan29@gmail.com>
2024-07-30 16:56:10 +0600

    headers and cookies in next middleware

--- prov_learning/keri_project/keri-front/src/middleware.ts
+++ prov_learning/keri_project/keri-front/src/middleware.ts
@@ -3,6 +3,14 @@ import type { NextRequest } from 'next/server'
  
 // This function can be marked `async` if using `await` inside
 export function middleware(request: NextRequest) {
+
+    const response =  NextResponse.next();
+    const themePrefer = request.cookies.get('theme');
+    if(!themePrefer){
+      response.cookies.set("theme", "dark");
+    }
+    response.headers.set("Custom-Header", "Hello");
+    return response;
     // if(request.nextUrl.pathname === '/comments'){
     //   // return NextResponse.redirect(new URL('/routes/comments', request.url))
     //   return NextResponse.rewrite(new URL('/routes/comments', request.url))
