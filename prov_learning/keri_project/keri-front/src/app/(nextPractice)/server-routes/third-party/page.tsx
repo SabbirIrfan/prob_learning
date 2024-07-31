ddfa9a59b84b78b8488855cd3541f448396a37cd
sabbirirfan <sabbirirfan29@gmail.com>
2024-07-31 16:58:26 +0600

    use of third party client component in server component

---
+++ prov_learning/keri_project/keri-front/src/app/(nextPractice)/server-routes/third-party/page.tsx
@@ -0,0 +1,13 @@
+import "server-only"
+import ImageSlider from "../../component/image-slider";
+export default function ThirdPartyPage() {
+ 
+    return (
+    <>
+        <h1>Third Party</h1>
+        <ImageSlider/>
+    </>
+    )
+
+}
+
